6608c49bcfbe07e63354dc6706324ffd8116cca1
Amrit Tamang <amttmg@gmail.com>
2024-11-20 14:33:58 +0545

    users

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard');
 });
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('projects', 'projects')
+    ->middleware(['auth', 'verified'])
+    ->name('projects');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__ . '/auth.php';
